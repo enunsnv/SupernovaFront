388c0696083f00722fe0c0bbbeb9066228a90680
trv449 <donghyun0215@naver.com>
2024-07-21 21:38:45 +0900

    style : 모달 변경

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,158 +3,174 @@ import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { userIDState } from '../atom/atom';
 import styled from 'styled-components';
-import LinkInput from '../components/TimeTable/LinkInput';
+import LinkInputModal from '../components/TimeTable/LinkInput'; // 수정된 LinkInputModal import 경로
+import PetSelect from '../components/pet/PetSelect'; // PetSelect import 경로 추가
 import Navbar from '../components/navigation/Navbar';
 
 const AppContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 100vh;
-  justify-content: space-between;
-  background-color: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    justify-content: space-between;
+    background-color: #fff;
 `;
 
 const Header = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-  position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    position: relative;
 `;
 
 const ToggleButton = styled.button`
-  width: 30px;
-  height: 30px;
-  background: url('/img/toggle.svg') no-repeat center center;
-  background-size: cover;
-  border: none;
-  cursor: pointer;
+    width: 30px;
+    height: 30px;
+    background: url('/img/toggle.svg') no-repeat center center;
+    background-size: cover;
+    border: none;
+    cursor: pointer;
 `;
 
 const SemesterInfo = styled.div`
-  font-size: 17px;
-  font-weight: 500;
-  color: #000;
-  position: absolute;
-  right: 20px;
-  flex-direction: row;
+    font-size: 17px;
+    font-weight: 500;
+    color: #000;
+    position: absolute;
+    right: 20px;
+    flex-direction: row;
 `;
 const Semester = styled.div`
-  font-size: 17px;
-  font-weight: 500;
-  color: #000;
-  position: absolute;
-  right: 20px;
-  flex-direction: row;
+    font-size: 17px;
+    font-weight: 500;
+    color: #000;
+    position: absolute;
+    right: 50px;
+    flex-direction: row;
 `;
 
 const Content = styled.div`
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center; 
-  justify-content: center; 
-  width: 100%;
-  padding: 0 20px; 
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 0 20px;
 `;
 
 const Info = styled.div`
-  width: 100%; 
-  padding: 0 20px; 
-  text-align: left; 
-  margin-left: 80px;
+    width: 100%;
+    padding: 0 20px;
+    text-align: left;
+    margin-left: 80px;
 `;
 
 const InfoText1 = styled.p`
-  color: #000;
-  font-size: 17px;
-  text-align: left;
-  margin-bottom: 4px;
+    color: #000;
+    font-size: 17px;
+    text-align: left;
+    margin-bottom: 4px;
 `;
 
 const InfoText2 = styled.p`
-  font-size: 24px; 
-  text-align: left;
-  font-weight: bold;
-  margin-top: 4px;
-  margin-bottom: 14px;
+    font-size: 24px;
+    text-align: left;
+    font-weight: bold;
+    margin-top: 4px;
+    margin-bottom: 14px;
 `;
 
 const InfoText3 = styled.p`
-  color: #555555;
-  font-size: 13px;
-  text-align: left;
-  margin-top: 14px;
+    color: #555555;
+    font-size: 13px;
+    text-align: left;
+    margin-top: 14px;
 `;
 
 const HighlightedText = styled.span`
-  color: #F27200;
-  font-weight: bold;
-  font-size: 28px;
+    color: #F27200;
+    font-weight: bold;
+    font-size: 28px;
 `;
 
 const Placeholder = styled.div`
-  width: 150px;
-  height: 150px;
-  background-color: #f5f5f5;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 20px;
-  border-radius: 10px;
+    width: 150px;
+    height: 150px;
+    background-color: #f5f5f5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    border-radius: 10px;
 `;
 
 const PlaceholderText = styled.p`
-  color: #b0b0b0;
+    color: #b0b0b0;
 `;
 
 const Footer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  padding: 10px 0;
-  background-color: #ffe680;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    padding: 10px 0;
+    background-color: #ffe680;
 `;
 
 function Main() {
-  const [userID, setUserID] = useRecoilState(userIDState);
-  const navigate = useNavigate();
-  const [showLinkInput, setShowLinkInput] = useState(false);
+    const [userID, setUserID] = useRecoilState(userIDState);
+    const navigate = useNavigate();
+    const [isLinkInputModalOpen, setIsLinkInputModalOpen] = useState(false);
+    const [isPetSelectOpen, setIsPetSelectOpen] = useState(false);
 
-  return (
-    <AppContainer>
-      <Header>
-        <ToggleButton onClick={() => setShowLinkInput(!showLinkInput)} />
-        {showLinkInput && <LinkInput />}
-        <SemesterInfo>
-          <Semester>0000</Semester>학년도 <Semester>0</Semester>학기
-        </SemesterInfo>
-      </Header>
-      
-      <Content>
-        <Info>
-          <InfoText1>
-            이번 주 공강 N시간 중
-          </InfoText1>
-          <InfoText2>
-            <HighlightedText>N시간</HighlightedText> 을 활용했어요
-          </InfoText2>
-          <InfoText3>
-            지난 주 대비 N%
-          </InfoText3>
-        </Info>
-        <Placeholder>
-          <PlaceholderText>펫 보이는 곳..</PlaceholderText>
-        </Placeholder>
-      </Content>
+    const handleSuccess = () => {
+        setIsPetSelectOpen(false);
+        setIsLinkInputModalOpen(true);
+    };
 
-      <Footer>
-        <Navbar />
-      </Footer>
-    </AppContainer>
-  );
+    return (
+        <AppContainer>
+            <Header>
+                <ToggleButton onClick={() => setIsLinkInputModalOpen(true)} />
+                <LinkInputModal
+                    open={isLinkInputModalOpen}
+                    onClose={() => setIsLinkInputModalOpen(false)}
+                    onSuccess={handleSuccess}
+                />
+                <PetSelect
+                    open={isPetSelectOpen}
+                    onClose={() => setIsPetSelectOpen(false)}
+                    onSuccess={handleSuccess}
+                />
+                <SemesterInfo>
+                    <Semester>0000</Semester>학년도 <Semester>0</Semester>학기
+                </SemesterInfo>
+            </Header>
+
+            <Content>
+                <Info>
+                    <InfoText1>
+                        이번 주 공강 N시간 중
+                    </InfoText1>
+                    <InfoText2>
+                        <HighlightedText>N시간</HighlightedText> 을 활용했어요
+                    </InfoText2>
+                    <InfoText3>
+                        지난 주 대비 N%
+                    </InfoText3>
+                </Info>
+                <Placeholder>
+                    <PlaceholderText>펫 보이는 곳..</PlaceholderText>
+                </Placeholder>
+            </Content>
+
+            <Footer>
+                <Navbar />
+            </Footer>
+        </AppContainer>
+    );
 }
 
 export default Main;
