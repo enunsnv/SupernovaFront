cde887889ecc09185130d514c49758afaf0e6af5
enunsnv <seoej0315@naver.com>
2024-07-21 17:36:21 +0900

    feat: 로그인 페이지 구현 완료

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const Container = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  height: 100%;
+  width: 100%;
   margin: 0;
+  background-color: white;
 `;
 
 const FormContainer = styled.div`
   background-color: white;
   border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   padding: 40px;
   width: 100%;
+  max-width: 400px;
   text-align: center;
   position: relative;
+  height: 100vh; 
 `;
 
 const Title = styled.h1`
   margin-bottom: 30px;
   font-size: 24px;
   color: #333;
+  margin: 100px 0;
 `;
 
 const Input = styled.input`
-  width: 100%;
-  padding: 15px;
+  width: 95%;
+  padding: 20px;
   margin: 10px 0;
   border: none;
   border-radius: 10px;
@@ -35,13 +38,15 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-  background-color: orange;
-  color: white;
+  background-color: #FFDA69;
+  color: black;
   border: none;
-  padding: 10px;
+  padding: 15px;
   border-radius: 5px;
   cursor: pointer;
-  margin-top: 20px;
+  margin-top: 100px;
+  width: 100px;
+  font-size: 15px;
 
   &:hover {
     background-color: darkorange;
@@ -52,33 +57,31 @@ const SignupButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 10px;
+  gap: 10px;
 `;
 
 const ShapeTop = styled.img`
   position: absolute;
-  top: 0px;
   right: -10px;
 `;
 
 const ShapeBottom = styled.img`
   position: absolute;
-  bottom: 0;
-  left: -50px;
+  left: -10px;
 `;
 
 const Login = () => {
   return (
     <Container>
       <FormContainer>
-        <ShapeTop src='/img/top-orange.svg' />
+        <ShapeTop src='/img/top-orange.svg' alt="top shape" />
         <Title>서비스명..</Title>
         <Input type="text" placeholder="닉네임" />
         <Input type="password" placeholder="비밀번호" />
         <SignupButtonContainer>
-        <Button>회원가입</Button>
+          <Button>로그인</Button>
         </SignupButtonContainer>
-        <ShapeBottom src='img/bottom-orange.svg' />
+        <ShapeBottom src='/img/bottom-orange.svg' alt="bottom shape" />
       </FormContainer>
     </Container>
   );
